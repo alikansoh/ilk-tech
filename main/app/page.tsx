c993e79e692b2a0253e46afbd7e4bf482783fdc9
alikansoh <alikanso.dev@gmail.com>
2026-04-29 14:41:37 +0100

    link the form with the email

--- main/app/page.tsx
+++ main/app/page.tsx
@@ -53,7 +53,7 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <HeroSection />
+      {/* <HeroSection /> */}
         <LogoTicker />
         <WhoWeAre />
         <OurProducts />
